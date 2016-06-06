21cf9b4936673b5b7f6090c9749667082ed21d88
WeekThor <vitktos@gmail.com>
2016-06-06 16:10:16 +0500

    Перевод

--- src/EssentialsPE/Tasks/Updater/UpdateFetchTask.php
+++ src/EssentialsPE/Tasks/Updater/UpdateFetchTask.php
@@ -24,35 +24,18 @@ class UpdateFetchTask extends AsyncTask{
 
     public function onRun(){
         switch($this->build){
-            case "stable":
-            default:
-                $url = "http://forums.pocketmine.net/api.php?action=getResource&value=886"; // PocketMine repository for 'Stable' releases
-                $this->build = "stable"; // Override property in case of an 'unknown' source
-                break;
             case "beta":
-                $url = "https://api.github.com/repos/LegendOfMCPE/EssentialsPE/releases"; // Github repository for 'Beta' releases
-                break;
-            case "development":
-                $url = "https://api.github.com/repos/LegendOfMCPE/EssentialsPE/contents/plugin.yml"; // Github repository for 'Development' versions
+                $url = "https://api.github.com/repos/WeekThor/EssentialsPE/releases"; // Github repository for 'Beta' releases
                 break;
         }
         $i = json_decode(Utils::getURL($url), true);
 
         $r = [];
         switch(strtolower($this->build)){
-            case "stable":
-                $r["version"] = $i["version_string"];
-                $r["downloadURL"] = "http://forums.pocketmine.net/plugins/essentialspe.886/download?version=" . $i["current_version_id"];
-                break;
             case "beta":
                 $i = $i[0]; // Grab the latest version from Github releases... Doesn't matter if it's Beta or Stable :3
                 $r["version"] = substr($i["name"], 13);
                 $r["downloadURL"] = $i["assets"][0]["browser_download_url"];
-                break;
-            case "development":
-                $content = yaml_parse(base64_decode($i["content"]));
-                $r["version"] = $content["version"];
-                $r["downloadURL"] = "https://github.com/LegendOfMCPE/EssentialsPE/raw/travis-build/EssentialsPE.phar";
                 break;
         }
         $this->setResult($r);
@@ -74,13 +57,13 @@ class UpdateFetchTask extends AsyncTask{
             $message = TextFormat::AQUA . "[EssentialsPE]" . TextFormat::GREEN .
                 ($this->build === "development" ?
                     "Fetching latest EssentialsPE development build..." :
-                    " A new " . TextFormat::YELLOW . $this->build . TextFormat::GREEN . " version of EssentialsPE found!"
+                    " Новая " . TextFormat::YELLOW . $this->build . TextFormat::GREEN . " Версия найдена!!"
                 ) .
-                " Version: " . TextFormat::YELLOW . $v . TextFormat::GREEN .
-                ($this->install !== true ? "" : ", " . TextFormat::LIGHT_PURPLE . "Installing...");
+                " Версия: " . TextFormat::YELLOW . $v . TextFormat::GREEN .
+                ($this->install !== true ? "" : ", " . TextFormat::LIGHT_PURPLE . "Установка...\n&4Помните, что у новой версии может не быть перевода");
         }else{
             $continue = false;
-            $message = TextFormat::AQUA . "[EssentialsPE]" . TextFormat::YELLOW . " No new version found, you're using the latest version of EssentialsPE";
+            $message = TextFormat::AQUA . "[EssentialsPE]" . TextFormat::YELLOW . " Нет новых релизов, у вас последняя версия(:";
         }
         $ess->getAPI()->broadcastUpdateAvailability($message);
         if($continue && $this->install){
